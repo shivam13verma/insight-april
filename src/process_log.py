cef5a926dbcde2d1b0f56485b730f6c99444ec41
vermashivam <shivam13verma@gmail.com>
2017-04-05 23:44:51 -0400

    code changes

--- src/process_log.py
+++ src/process_log.py
@@ -1,26 +1,33 @@
+###Author: Shivam Verma###
+
 import sys
 import re
 from datetime import timedelta
 from datetime import datetime
-from collections import defaultdict, deque
+from operator import itemgetter, attrgetter
 
 def run_feature1(out1_path, inp_list):
     """
     Feature 1: List in descending order of the top 10 most active hosts/IP addresses that have accessed the site.
     """
-    hosts = {}
-    for inp in inp_list:
+    hosts = {} #looks like {host1: count1, ...}
+    for inp in inp_list: #for each request
         try:
             if hosts[inp[0]]:
-                hosts[inp[0]] += 1
+                hosts[inp[0]] += 1 #update host-count dictionary
         except KeyError:
             hosts[inp[0]] = 1
 
-    sorted_hosts = sorted(hosts.items(), key=lambda x: x[1], reverse=True)
+    #lexicographic sort by host counts
+    sorted_hosts = sorted(hosts.items(), key=lambda x: (-x[1],x[0]))
+    #key=lambda x: x[1], reverse=True)
 
     with open(out1_path,'wb') as f:
         for i in range(10):
-            f.write(sorted_hosts[i][0]+","+str(sorted_hosts[i][1])+"\n")
+            try:
+                f.write(sorted_hosts[i][0]+","+str(sorted_hosts[i][1])+"\n")
+            except IndexError:
+                pass
 
 
 def run_feature2(out2_path, inp_list):
@@ -28,29 +35,41 @@ def run_feature2(out2_path, inp_list):
     Feature 2: Identify the top 10 resources on the site that consume the most bandwidth.
     """
 
-    resources = {}
-    for inp in inp_list:
-        res = inp[2].split(" ")[1] #resource
+    resources = {} #looks like {resource1: bandwidth1, ...}
+    for inp in inp_list: #for each request
+        try:
+            res = inp[2].split(" ")[1] #get resource from request
+        except IndexError:
+            print "faulty input: ",inp
+            res = inp[2].split(" ")[0] #resource not present
         byte = int(inp[-1])
         try:
             if resources[res]:
-                resources[res] += byte
+                resources[res] += byte #update resource-bandwidth dictionary
         except KeyError:
             resources[res] = byte
 
-    sorted_resources = sorted(resources.items(), key=lambda x: x[1], reverse=True)
+    #lexicographic sort by bandwidth
+    sorted_resources = sorted(resources.items(), key=lambda x: (-x[1],x[0]))
 
     with open(out2_path,'wb') as f:
         for i in range(10):
-            f.write(sorted_resources[i][0]+"\n")
+            try:
+                f.write(sorted_resources[i][0]+"\n")
+            except IndexError:
+                pass
 
 def convert_to_datetime(ts):
+    """
+    Converts timestamp string to datetime object.
+    """
     formats = ['%d/%b/%Y:%H:%M:%S','%d/%b/%Y:%H:%M', '%d/%b/%Y:%H', '%d/%b/%Y']
     ts = ts.split(" ")[0].rstrip(":") #remove timezone
     out = datetime(1111, 1, 1, 0, 0, 0) #default time if nothing works
     for f in formats:
         try:
-            out = datetime.strptime(ts, f)
+            t = datetime.strptime(ts, f)
+            out = datetime(t.year, t.month, t.day, t.hour, t.minute, t.second) #make sure HH:MM:SS is present
             break
         except ValueError:
             continue
@@ -64,7 +83,8 @@ def run_feature3(out3_path, inp_list):
 
     timestamps = map(lambda x: convert_to_datetime(x[1]), inp_list) #remove timezone, get list
     sorted_timestamps = sorted(timestamps) #sort by timestamp in increasing order
-    start_times = {}
+
+    start_times = {} #looks like {time1: count1 ...}
     curr_time = sorted_timestamps[0]
     end_time = sorted_timestamps[-1]
 
@@ -76,20 +96,28 @@ def run_feature3(out3_path, inp_list):
         cutoff = curr_time + timedelta(minutes=60) #60-min. cutoff for curr_time
 
         for x in sorted_timestamps: #for each sorted timestamp
-            if x <= cutoff: #if below cutoff
-                start_times[curr_time] += 1
+            if x >= curr_time:
+                if x <= cutoff: #if below cutoff
+                    start_times[curr_time] += 1 #update time-count dictionary
+                else:
+                    break
             else:
-                break
+                continue
 
         curr_time = curr_time + timedelta(seconds=1) #update current time
 
-
-    sorted_time_counts = sorted(start_times.items(), key=lambda x: x[1], reverse=True)
+    #lexicographic sort by time counts (reverse order), then datetime (increasing order)
+    sorted_time_counts = sorted(start_times.items(), key=lambda x: (-x[1],x[0]))
     #looks like [(time1, count1), (time2, count2), ...]
 
     with open(out3_path,'wb') as f:
         for i in range(10):
-            f.write(sorted_time_counts[i][0]+"\n")
+            try:
+                t = sorted_time_counts[i][0]
+                dt = datetime.strftime(t, '%d/%b/%Y:%H:%M:%S') + " -0400"
+                f.write(dt+","+str(sorted_time_counts[i][1])+"\n")
+            except IndexError:
+                pass
 
 
 def run_feature4(out4_path, inp_list, inp):
@@ -98,13 +126,13 @@ def run_feature4(out4_path, inp_list, inp):
     """
 
     ip_ts = map(lambda x: (x[0], convert_to_datetime(x[1]), int(x[3])), inp_list) #remove timezone, get list
-    zipped_ip = zip(range(len(ip_ts)), ip_ts)
-    sorted_ip = sorted(ip_ts, key = lambda x: x[1][1])
-    sort_order = map(lambda x: x[0], sorted_ip)
+    zipped_ip = zip(range(len(ip_ts)), ip_ts) #zip to add index
+    sorted_ip = sorted(zipped_ip, key = lambda x: x[1][1]) #sort by datetime
+    sort_order = map(lambda x: x[0], sorted_ip) #get sorted indices
 
-    blocked = {} #{ip1: block_end_time1, ...}
-    ip_strikes = {} #{ip1: [strike_end_time1, strikes]}
-    out_blocked = []
+    blocked = {} #looks like {ip1: block_end_time1, ...}
+    ip_strikes = {} #looks like {ip1: [strike_end_time1, num_strikes]}
+    out_blocked = [] #output
 
     for i in sort_order: #iterate over time-sorted login queries
 
@@ -129,17 +157,17 @@ def run_feature4(out4_path, inp_list, inp):
 
                 #case 2a: within 20 seconds of first strike
                 if curr_login[1] <= ip_strikes[curr_login[0]][0]:
-                    ip_strikes[curr_login[0]][1] += 1
+                    ip_strikes[curr_login[0]][1] += 1 #add strike
 
                     #three strikes are done, add IP to block list
                     if ip_strikes[curr_login[0]][1] == 3:
-                        blocked[curr_login[0]] = curr_login[1] + timedelta(minutes=5)
+                        blocked[curr_login[0]] = curr_login[1] + timedelta(minutes=5) #create block and start 5-minute timer
 
                 else: #case 2b: 20 seconds have passed
                     ip_strikes.pop(curr_login[0], None) #remove IP from strike list
 
             else: #case 3: failed IP not in block or strike list i.e. first strike
-                ip_strikes[curr_login[0]] = [curr_login[1] + timedelta(seconds=20), 1]
+                ip_strikes[curr_login[0]] = [curr_login[1] + timedelta(seconds=20), 1] #create strike and start 20-second timer
 
         else: #case 4: IP is successful
 
@@ -150,8 +178,8 @@ def run_feature4(out4_path, inp_list, inp):
                         out_blocked.append(inp[i]) #add to blocked.txt
 
                     else: #case 2: IP is in block list but 5 minutes passed
-                        blocked.pop(curr_login[0], None)
-                        ip_strikes.pop(curr_login[0], None)
+                        blocked.pop(curr_login[0], None) #remove IP from block list
+                        ip_strikes.pop(curr_login[0], None) #remove IP from strike list
 
                 #case 4b: IP not blocked but in strike list
                 elif curr_login[0] in ip_strikes:
@@ -173,22 +201,12 @@ def main():
     for arg in sys.argv:
         args.append(arg)
 
+    #read input and output paths
     input_path = str(args[1])
     out1_path = str(args[2])
     out2_path = str(args[3])
     out3_path = str(args[4])
     out4_path = str(args[5])
-    #out5_path = str(args[7]) #uncomment this when running feature5.
-    #out6_path = str(args[8]) #uncomment this when running feature6.
-
-    #batch_path = './paymo_input/batch_payment.txt'
-    #stream_path = './paymo_input/stream_payment.txt'
-    #out1_path = './paymo_output/output1.txt'
-    #out2_path = './paymo_output/output2.txt'
-    #out3_path = './paymo_output/output3.txt'
-    #out4_path = './paymo_output/output4.txt'
-    #out5_path = './paymo_output/output5.txt'
-    #out5_path = './paymo_output/output6.txt'
 
     inp = []
     with open(input_path,'r') as f:
@@ -196,10 +214,12 @@ def main():
             inp.append(line)
 
     def parse_line(line):
-
+        """
+        Parses input request into corresponding fields below.
+        """
         line = line.rstrip("\n").rstrip().lstrip()
         host = line.split("- -")[0].rstrip().lstrip()
-        timestamp = line[line.find("[")+1:test.find("]")]
+        timestamp = line[line.find("[")+1:line.find("]")]
         request = re.findall('"([^"]*)"', line)[0]
         http = line.split(" ")[-2]
         byte = line.split(" ")[-1]
@@ -209,15 +229,13 @@ def main():
         return [host, timestamp, request, http, byte]
 
 
-    inp_list = map(lambda x: parse_line(x), inp) #contains list of form [['id1','id2'],['id3','id4'],...]
+    inp_list = map(lambda x: parse_line(x), inp) #parses input
 
+    #run features/analytics
     run_feature1(out1_path, inp_list)
     run_feature2(out2_path, inp_list)
     run_feature3(out3_path, inp_list)
-    run_feature4(out3_path, inp_list, inp)
-    #run_feature4(out4_path, stream_list, batch_dict, k=10) #uncomment this when running feature4.
-    #run_feature5(out5_path, stream, batch, last_transaction = 365) #uncomment this when running feature5.
-    #run_feature5(out6_path, stream, batch, mult_factor = 1.5) #uncomment this when running feature6.
+    run_feature4(out4_path, inp_list, inp)
 
 if __name__ == '__main__':
     main()
